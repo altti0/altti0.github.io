d639d9c73e975ddd1cb1e6782eefebb8af3bc183
altti0 <altti0@gmail.com>
2015-06-17 16:00:15 +0900

    Update and rename CoP_part2_edgePreload.js to index_edge.js

---
+++ index_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x5='rgba(94,52,116,1.00)',x4='rgba(105,199,143,1.00)',xc='rgba(0,0,0,1)',m='rect',i='none';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Rectangle',t:m,r:['125px','177px','45px','45px','auto','auto'],f:[x4],s:[0,xc,i]}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'550px','400px'],overflow:'hidden',f:[x5]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-26728627");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-26728627");
